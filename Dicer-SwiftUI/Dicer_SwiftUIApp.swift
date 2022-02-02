62ee42f4431a75f369e6563251757d04e23192ba
Edgar Solis <edgarx75@gmail.com>
2022-02-02 13:27:29 -0600

    Initial Commit

---
+++ Dicer-SwiftUI/Dicer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicer_SwiftUIApp.swift
+//  Dicer-SwiftUI
+//
+//  Created by Edgar Solis on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
